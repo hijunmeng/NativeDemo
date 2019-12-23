9e6b81a87a1423ef63e891d59d56ade0dc56c025
hwj <304797017@qq.com>
2019-12-23 23:08:28 +0800

    Initial commit

---
+++ app/src/main/java/com/junmeng/nativedemo/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.junmeng.nativedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.junmeng.libnative.JniNative
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        sample_text.text = JniNative.getInstance()?.stringFromJNI()
+    }
+
+
+}
---
+++ lib_native/src/main/java/com/junmeng/libnative/JniNative.kt
@@ -0,0 +1,32 @@
+package com.junmeng.libnative
+
+
+class JniNative private constructor(){
+
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+        private var instance: JniNative? = null
+
+        fun getInstance(): JniNative? {
+            if (instance == null) {
+                synchronized(JniNative::class.java) { instance = JniNative() }
+            }
+            return instance
+        }
+    }
+
+
+
+
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+}
